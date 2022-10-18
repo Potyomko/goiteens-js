c508a67bfce78ca1a0ba58573dcddbd214ebaf20
Potyomko Veronika <vnpotomko@gmail.com>
2022-10-18 20:38:20 +0300

    Класна 18 жовтня

---
+++ 18.10.22.js
@@ -0,0 +1,12 @@
+const name = 'генератор захисного поля'
+let price = 1000;
+console.log(Обрано ${name}, ціна за штуку ${price});
+
+
+const total = 100;
+const ordered = 50;
+if (total < ordered) {
+    console.log("На складі недостатньо товарів!");
+} else {
+    console.log("Замовлення оформлено, з вами зв'яжеться менеджер");
+}
