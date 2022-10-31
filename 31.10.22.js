b5ade67bf3ab0d14fa30860565e4027aa93add33
Potyomko Veronika <vnpotomko@gmail.com>
2022-10-31 17:06:27 +0200

    Класна 27 жовтня і менторська 31 жовтня

---
+++ 31.10.22.js
@@ -0,0 +1,14 @@
+// const message = "[SPAM] How to earn fast money?"
+// console.log(message.includes('spam'));
+// const messageNormalize = message.toLocaleLowerCase();
+// console.log(messageNormalize.includes('spam'));
+
+
+// const message = "Curabitur ligula sapien";
+// const maxlenghs = 16;
+// if (massage.length <= maxlenghs ) {
+//     console.log(massage);
+// } else {
+//     const newMassage = massage.split('').splice(maxlenghs, )
+
+// }
